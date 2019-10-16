1f581984116e7a81f6ec3ee7482754368bc2c17d
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-16 05:16:42 -0500

    setup randomizers for carScore properties, pushed thsoe values to html for parsing later on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,40 @@
-<script src = "https://code.jquery.com/jquery-3.4.1.slim.js"></script>
-
-let playerwin = 0;
-let playerlose = 0;
-let targetscore = 0;
-let playerscore = 0;
-
-function randomNum(){
-    Math.floor(Math.random()*12);
+let playerWin = 0;
+let playerLose = 0;
+let targetScore = 0;
+let playerScore = 0;
+const carScore = {
+    bmw: 0,
+    nissan: 0,
+    rx7: 0,
+    skyline: 0,
 }
 
 function scoreBoard(){
-    $("#score").html("Score: " + playerscore);
-    $("#targetscore").html("Target: " + targetscore);
-    $("#wins").html("Wins: " + playerwin);
-    $("#loses").html("Lose: " + playerlose);
+    $("#Score").html("Score: " + playerScore);
+    $("#Target-Score").html("Target: " + targetScore);
+    $("#Wins").html("Wins: " + playerWin);
+    $("#Losses").html("Losses: " + playerLose);
 }
+
+$("#image").on("touch click keyup", function(){
+
+})
+
+function pagePrep(){
+    carScore.bmw = Math.floor(Math.random()*12 + 1);
+    $("#bmw-image").attr("value", carScore.bmw);
+    carScore.nissan = Math.floor(Math.random()*12 + 1);
+    $("#nissan-image").attr("value", carScore.nissan);
+    carScore.rx7 = Math.floor(Math.random()*12 + 1);
+    $("#rx7-image").attr("value", carScore.rx7);
+    carScore.skyline = Math.floor(Math.random()*12 + 1);
+    $("#skyline-image").attr("value", carScore.skyline);
+    targetScore = Math.floor(Math.random()*120 +1);
+}
+
+pagePrep();
+scoreBoard();
+
+
+console.log(carScore);
+console.log(targetScore);
